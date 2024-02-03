078709faf9197ae4f7acd6cd14e63fbeb5253e77
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2024-02-03 14:24:57 +0300

    Table pagination and search complete.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +9,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatagridComponent } from './components/datagrid/datagrid.component';
 
 import { DxDataGridModule } from 'devextreme-angular';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const COMPONENTS = [NavbarComponent, DatagridComponent];
 const SERVICES = [];
@@ -19,7 +22,10 @@ const SERVICES = [];
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
+    FontAwesomeModule,
     DxDataGridModule
   ],
   providers: [],
--- src/app/components/datagrid/datagrid.component.ts
+++ src/app/components/datagrid/datagrid.component.ts
@@ -1,16 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TableData } from '../../models/table-data.model';
 import { TableService } from '../../services/table.service';
-
+import { faCaretDown, faCaretUp, faFilter } from '@fortawesome/free-solid-svg-icons';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DxDataGridComponent } from 'devextreme-angular';
 @Component({
   selector: 'datagrid',
   templateUrl: './datagrid.component.html',
   styleUrl: './datagrid.component.css'
 })
-export class DatagridComponent {
+export class DatagridComponent implements OnInit {
+  @ViewChild('grid', { static: false }) grid!: DxDataGridComponent;
   tableData: TableData[] = [];
+  displayedData: TableData[] = [];
 
-  constructor(service: TableService) {
+  faFilter = faFilter;
+  faUp = faCaretUp;
+  faDown = faCaretDown;
+
+  searchQuery: string = '';
+  currentPageSize: number = 4;
+  currentPageIndex: number = 0;
+  get totalNumberOfPages() {
+    return Math.ceil(this.tableData.length / this.currentPageSize);
+  }
+
+  private destroy$ = new Subject<void>();
+  myForm!: FormGroup;
+  
+
+  constructor(service: TableService, private fb: FormBuilder) {
     this.tableData = service.getTableData();
+    this.myForm = this.fb.group({
+      searchText: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.displayedData = [...this.tableData];
+    this.myForm.get('searchText')?.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe((textValue: string) => {
+      const searchQuery = textValue
+      this.searchTableByText(searchQuery)
+    });
+  }
+
+  searchTableByText(input: string) {
+    this.displayedData = this.tableData.filter(row =>
+      Object.values(row).some(value =>
+        value.toString().toLowerCase().includes(input.toLowerCase())
+      )
+    );
+  }
+  
+
+  increasePageSize() {
+    this.currentPageSize++;
+  }
+
+  decreasePageSize() {
+    if(this.currentPageSize > 4) {
+      this.currentPageSize--;
+    }
+  }
+
+  setCurrentPage(event: any) {
+    event.data && (this.currentPageIndex = event.data - 1)
+  }
+
+  previousPage() {
+    if(this.currentPageIndex > 0) {
+      this.currentPageIndex--;
+    }
+  }
+
+  nextPage() {    
+    if(this.currentPageIndex < (this.totalNumberOfPages - 1)) {
+      this.currentPageIndex++;
+    }
+  }
+
+  openModal() {
+
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
