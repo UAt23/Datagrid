d8010ef655254a3c28d8731c28e3ce7c88544a4f
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2024-02-03 03:11:51 +0300

    UI development is almost done.

--- src/app/models/table-data.model.ts
+++ src/app/models/table-data.model.ts
@@ -1,18 +1,5 @@
 export interface TableData {
-   EmployeeID: Number,
-   FullName: String,
-   Position: String,
-   TitleOfCourtesy: String,
-   BirthDate: String,
-   HireDate: String,
-   Address: String,
-   City: String,
-   Region: String,
-   PostalCode: String,
-   Country: String,
-   HomePhone: String,
-   Extension: String,
-   Photo: String,
-   Notes: String,
-   ReportsTo: Number | null
+   'Sosyal Medya Linki': string,
+   'Sosyal Medya Adı': string,
+   'Açıklama': string
 }
